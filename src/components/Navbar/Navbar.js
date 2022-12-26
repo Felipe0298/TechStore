6f8d2e52bc6315050b55e80789dc0b4dcc15aa56
Felipe Hoyos <felipe02-98@hotmail.com>
2022-12-26 12:06:55 -0500

    Primera Entrega

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,20 @@
 import CartWidget from "../cartWidget/cartWidget";
-import './Navbar.css'
+import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="Navbar">
       <h1 className="h1__Navbar">TechStore</h1>
 
-    <div className="div__Navbar">
-    <div>
-        <button>Inicio</button>
-        <button>Samsung</button>
-        <button>LG</button>
-        <button>Apple</button>
-        <button>Contacto</button>
+      <div className="div__Navbar">
+        <div className="div__Navbar">
+          <button>Inicio</button>
+          <button>Samsung</button>
+          <button>LG</button>
+          <button>Apple</button>
+          <button>Contacto</button>
+        </div>
+        <CartWidget />
       </div>
-      <CartWidget />
-    </div>
-      
-      
     </nav>
   );
 };
